93af8c820e42be2aaa32674bff1dbe2ef5d8ff56
gilang-tectona <gilangtectona1.6@gmail.com>
2026-01-21 10:27:44 +0700

    Switch database configuration to SQLite

--- menu/settings.py
+++ menu/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'menu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_base',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',  # nanti diisi dari PA
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'django_base',
+#        'USER': 'postgres',
+#        'PASSWORD': '12345',
+#        'HOST': 'localhost',  # nanti diisi dari PA
+#        'PORT': '5432',
+#    }
+#}
 
 
 # Password validation
